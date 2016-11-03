0419981fb6eb5b750c5f933e608292dac7e0a036
YIBING <yibing@zbj.com>
2016-11-03 13:50:02 +0800

    ubuntu auto mirrors with automatical codename

--- ubuntu-auto-mirrors.sh
+++ ubuntu-auto-mirrors.sh
@@ -5,5 +5,7 @@ set -x
 if [ ! -f /etc/apt/sources.list.bak ]
 then
   cp /etc/apt/sources.list /etc/apt/sources.list.bak
-  cat /vagrant/ubuntu-auto-mirros | tee /etc/apt/sources.list
+  cat /vagrant/ubuntu-auto-mirrors | tee /etc/apt/sources.list
+  CODE_NAME=$(lsb_release -a | grep -i codename | awk '{print $2}')
+  sed -i "s/CODE_NAME/$CODE_NAME/g" /etc/apt/sources.list
 fi
